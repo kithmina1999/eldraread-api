4994102a253ed04bfe03447e3e161902559eb1ab
kithmina1999 <kithgaming99@gmail.com>
2025-04-16 10:37:21 +0530

    chore:add cors configs

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/kithmina1999/eldraread-api/routes"
 )
 
-func main(){
-	app:= fiber.New()
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Content-Type,Authorization",
+	}))
 
 	routes.RegisterUserRoutes(app)
 
 	app.Listen(":8080")
 }
-
